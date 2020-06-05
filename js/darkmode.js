57446a85b07831d8edbc8b9593c939c1af7259cf
Gabriel Wong <gabriel.wong.au@gmail.com>
2020-06-05 16:26:23 +1000

    Fixed bugs

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,3 +1,28 @@
+//Script for dark mode-light mode toggle button
+
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+        window.localStorage.setItem('data-theme', 'light');
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+        window.localStorage.setItem('data-theme', 'dark');
+        }
+    })
+
+    let trans = () => {
+        document.documentElement.classList.add('transition');
+        window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
+
+//Script for storing state in local storage
+
 const toggleSwitch = document.getElementById('switch');
 const currentTheme = localStorage.getItem('data-theme');
 
